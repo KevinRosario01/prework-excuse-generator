2406743f4c39526cf937e3dcf91deb6a94e57ec8
Kevin <29806319+KevinRosario01@users.noreply.github.com>
2024-04-26 22:52:55 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoRDM = Math.floor(Math.random() * who.length);
+  let actionRDM = Math.floor(Math.random() * action.length);
+  let whatRDM = Math.floor(Math.random() * what.length);
+  let whenRDM = Math.floor(Math.random() * when.length);
+
+  let excuse =
+    who[whoRDM] +
+    " " +
+    action[actionRDM] +
+    " " +
+    what[whatRDM] +
+    " " +
+    when[whenRDM];
+  document.querySelector("#excuse").innerHTML = excuse;
 };
